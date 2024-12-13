abb60df5900a7998fa2c768caf258b5d7b73912e
Kauã Thierry <kauathierry86@gmail.com>
2024-12-13 18:14:53 -0300

    Codigo 95%

--- web/serviços/js/servicos.js
+++ web/serviços/js/servicos.js
@@ -1,3 +1,15 @@
+const closeModal = document.getElementById('closeModal')
+const modal = document.getElementById('modal')
+const openModal = document.getElementById('btn-modal')
+
+openModal.onclick = function(){
+  modal.showModal()
+}
+
+closeModal.onclick = function() {
+  modal.close()
+}
+
 /*=============== SWIPER JS ===============*/
 let swiperCards = new Swiper(".card__content", {
   loop: true,
@@ -15,7 +27,7 @@ let swiperCards = new Swiper(".card__content", {
     prevEl: ".button-prev",
   },
 
-  breakpoints:{
+  breakpoints: {
     600: {
       slidesPerView: 2,
     },
@@ -24,3 +36,5 @@ let swiperCards = new Swiper(".card__content", {
     },
   },
 });
+
+
